c290092f83e14543c496ab6e364fb589b7c773d9
tenajsystems <31551697+tenajsystems@users.noreply.github.com>
2021-10-15 11:55:38 -0400

    Update GravityZone.php

--- src/GravityZone.php
+++ src/GravityZone.php
@@ -7,11 +7,11 @@
 
 declare(strict_types=1);
 
-namespace IndianaUniversity\GravityZone;
+namespace TenajSystems\GravityZone;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
-use IndianaUniversity\GravityZone\Traits\{
+use TenajSystems\GravityZone\Traits\{
     AccountsTrait,
     GeneralTrait,
     NetworkTrait,
